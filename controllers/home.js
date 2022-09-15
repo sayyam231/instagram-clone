3b83a2a93fc7a13f1f36247a11c9470c89db31dc
sayyam231 <sayyamjain.it1@bvp.edu.in>
2022-09-15 13:17:59 +0530

    conver home controllerr to async await

--- controllers/home.js
+++ controllers/home.js
@@ -1,22 +1,18 @@
 const { populate } = require('../models/comments');
 const Post = require('../models/post');
-module.exports.home = (req, res) => {
-    Post.find({}).sort({ $natural: -1 })
-        .populate('user')
-        .populate({
-            path: 'comment',
-            populate:'user'
-        })
-        .exec(function (err, post) {
-        if (err) {
-            console.log('err in finding the post');
-        }
-        
+module.exports.home = async (req, res) => {
+    try {
+    let post = await Post.find({}).sort({ $natural: -1 })
+            .populate('user')
+            .populate({
+                path: 'comment',
+                populate: 'user'
+            });
         return res.render('home', {
             title: "Instagram",
-            posts : post
+            posts: post
         });
-
-    })
-   
+    } catch (error) {
+        console.log("Error", error);
+    }
 }
